24e1b5c632957cda168018437ab875bfa130c6f6
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-03 19:23:59 -0300

    Update import.py

--- import.py
+++ import.py
@@ -1,6 +1,6 @@
 import csv
 import os
-
+from flask import Flask, render_template, request, url_for, redirect, session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
