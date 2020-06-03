943a0639f3b508d524beb2c857f3d9fa84aaede6
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-03 19:09:44 -0300

    Create application.py

---
+++ application.py
@@ -0,0 +1,29 @@
+import csv
+import os
+
+from flask import Flask, render_template
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+@app.route("/")
+  def index():
+      flights = db.execute("SELECT * FROM flights").fetchall()
+      return render_template("indexflights.html", flights=flights)
+
+  @app.route("/book", methods=["POST"])
+  def book():
+      # Get form information.
+      name = request.form.get("name")
+      try:
+          flight_id = int(request.form.get("flight_id"))
+      except ValueError:
+          return render_template("error.html", message="Invalid flight number.")
+
+      # Make sure the flight exists.
+      if db.execute("SELECT * FROM flights WHERE id = :id", {"id": flight_id}).rowcount == 0:
+          return render_template("error.html", message="No such flight with that id.")
+      db.execute("INSERT INTO passengers (name, flight_id) VALUES (:name, :flight_id)",
+              {"name": name, "flight_id": flight_id})
+      db.commit()
+      return render_template("success.html")
