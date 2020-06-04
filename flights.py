703001fd34dec55a9d0fdbc830ff81425258648e
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-03 23:38:41 -0300

    Update flights.py

--- flights.py
+++ flights.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, url_for, redirect, session # 
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+
 app = Flask(__name__)
 
 app.config["SESSION_PERMANENT"] = False
