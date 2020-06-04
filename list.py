c22eb5f710ba3ea894171004fdb65fd0e5f1d14b
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-04 18:08:59 -0300

    Update list.py

--- list.py
+++ list.py
@@ -1,16 +1,15 @@
 import os
 
-  from sqlalchemy import create_engine
-  from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
-  engine = create_engine(os.getenv("DATABASE_URL")) # database engine object from SQLAlchemy that manages connections to the database
-                                                    # DATABASE_URL is an environment variable that indicates where the database lives
-  db = scoped_session(sessionmaker(bind=engine))    # create a 'scoped session' that ensures different users' interactions with the
-                                                    # database are kept separate
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 
-  flights = db.execute("SELECT origin, destination, duration FROM flights").fetchall() # execute this SQL command and return all of the results
-  for flight in flights
-      print(f"{flight.origin} to {flight.destination}, {flight.duration} minutes.") # for every flight, print out the flight info
+def main():
+    flights = db.execute("SELECT origin, destination, duration FROM flights").fetchall()
+    for flight in flights:
+        print(f"{flight.origin} to {flight.destination}, {flight.duration} minutes.")
 
-  if __name__ == "__main__":
-      main()
+if __name__ == "__main__":
+    main()
