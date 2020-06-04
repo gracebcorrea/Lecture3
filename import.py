d831e9e58e25e3b1d88018e72144266ba95a4228
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-04 18:08:38 -0300

    Update import.py

--- import.py
+++ import.py
@@ -1,21 +1,20 @@
 import csv
 import os
-from flask import Flask, render_template, request, url_for, redirect, session
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = creage_engine(os.getenv("DATABASE_URL"))
-db =scoped_session(sessionmaker(bind=engine))
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 
-  # same import and setup statements as above
 def main():
-  f = open("flights.csv")
-  reader = csv.reader(f)
-  for origin, destination, duration in reader: # loop gives each column a name
-      db.execute("INSERT INTO flights (origin, destination, duration) VALUES (:origin, :destination, :duration)",
-                  {"origin": origin, "destination": destination, "duration": duration}) # substitute values from CSV line into SQL command, as per this dict
-      print(f"Added flight from {origin} to {destination} lasting {duration} minutes.")
-  db.commit() # transactions are assumed, so close the transaction finished
+    f = open("flights.csv")
+    reader = csv.reader(f)
+    for origin, destination, duration in reader:
+        db.execute("INSERT INTO flights (origin, destination, duration) VALUES (:origin, :destination, :duration)",
+                    {"origin": origin, "destination": destination, "duration": duration})
+        print(f"Added flight from {origin} to {destination} lasting {duration} minutes.")
+    db.commit()
 
 if __name__ == "__main__":
     main()
