c9b6f699f79c65430d4c5b0ce87ebf243c378779
gracebcorrea <64984155+gracebcorrea@users.noreply.github.com>
2020-06-04 18:09:32 -0300

    Update passengers.py

--- passengers.py
+++ passengers.py
@@ -1,34 +1,36 @@
-import csv
 import os
-from flask import Flask, render_template, request, url_for, redirect, session
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = creage_engine(os.getenv("DATABASE_URL"))
-db =scoped_session(sessionmaker(bind=engine))
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 
-  # same import and setup statements as above
 def main():
-    #list all flights
-    flights = db.execute("SELECT id, origin, destination, duration FROM flight "),fetchall()
-    for flight in flights:
-        print(f"Flight {flight.id}: {flight.origin} to {flight.destination}, {flight.duration} minutes")
 
-    #Prompt user to choose a flight.
-    flight_id = int(input("\nFlight ID:"))
-    flight = db.execute("SELECT origin, destination, duration FROM flights WHERE id = :id ", )
+    # List all flights.
+    flights = db.execute("SELECT id, origin, destination, duration FROM flights").fetchall()
+    for flight in flights:
+        print(f"Flight {flight.id}: {flight.origin} to {flight.destination}, {flight.duration} minutes.")
+
+    # Prompt user to choose a flight.
+    flight_id = int(input("\nFlight ID: "))
+    flight = db.execute("SELECT origin, destination, duration FROM flights WHERE id = :id",
                         {"id": flight_id}).fetchone()
 
-    #make sure the flight is valid
+    # Make sure flight is valid.
     if flight is None:
         print("Error: No such flight.")
         return
 
-    #List passengers
+    # List passengers.
     passengers = db.execute("SELECT name FROM passengers WHERE flight_id = :flight_id",
                             {"flight_id": flight_id}).fetchall()
     print("\nPassengers:")
     for passenger in passengers:
         print(passenger.name)
-    if len(passengers) == o:
-        print("no passengers.")
+    if len(passengers) == 0:
+        print("No passengers.")
+
+if __name__ == "__main__":
+    main()
